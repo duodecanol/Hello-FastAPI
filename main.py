bd9370fc354bdb85a3098648dac9af651b1d6131
HOME-2700 <metalhwal@gmail.com>
2022-02-07 11:15:38 +0900

    part-01-hello world
    https://christophergs.com/tutorials/ultimate-fastapi-tutorial-pt-1-hello-world/

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 from fastapi import FastAPI
+import uvicorn
 
 class ModelName(str, Enum):
     alexnet = 'alexnet'
@@ -8,6 +9,8 @@ class ModelName(str, Enum):
     lenet = 'lenet'
 
 app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 @app.get("/")
 async def root():
@@ -38,3 +41,16 @@ async def get_model(model_name: ModelName): # Typing을 해주면 Enum 사용 �
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+# @app.get("/items/")
+# async def read_item(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip : skip + limit]
+
+@app.get("/items/{item_id}")
+async def read_itmes(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8800, reload=True)
