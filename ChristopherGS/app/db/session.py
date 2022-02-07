ec4c74c03a3989408c716f96f1c3b68b8f7f331f
HOME-2700 <metalhwal@gmail.com>
2022-02-07 13:47:07 +0900

    part-07-database [1/3]

---
+++ ChristopherGS/app/db/session.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///example.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URI,
+    # necessary to work with SQLite -
+    # this is a common gotcha because FastAPI can access the database
+    # with multiple threads during a single request,
+    # so SQLite needs to be configured to allow that.
+    connect_args={"check_same_thread": False},
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
